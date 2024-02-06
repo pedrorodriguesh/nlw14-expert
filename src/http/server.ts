2ebf20003ee129a305f14718df971710d992e49f
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-06 19:40:12 -0300

    registering route

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import fastify from "fastify"
 import { z } from 'zod'
 import { createPoll } from "./routes/create-poll"
 import { getPoll } from "./routes/get-poll"
+import { voteOnPoll } from "./routes/vote-on-poll"
 
 const app = fastify()
 
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333}).then(() => {
   console.log(`Server is running on port 3333`)
