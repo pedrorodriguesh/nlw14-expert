b126b9b4c37e509bc3a6081ebb8418ea4a96b86d
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-06 19:11:34 -0300

    separando rotas

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,10 @@
-import { PrismaClient } from "@prisma/client"
 import fastify from "fastify"
 import { z } from 'zod'
+import { createPoll } from "./routes/create-poll"
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.post('/polls', async (request, reply) => {
-  const createPollBodySchema = z.object({
-    title: z.string(),
-  })
-
-  const { title } = createPollBodySchema.parse(request.body)
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    }
-  })
-
-  return reply.status(201).send({
-    pollId: poll.id
-  })
-
-})
+app.register(createPoll)
 
 app.listen({ port: 3333}).then(() => {
   console.log(`Server is running on port 3333`)
