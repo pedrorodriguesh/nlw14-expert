3ff1487654e5023424ca9fba6522f423752f55ed
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-06 19:55:03 -0300

    vote route and cookies

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,6 @@
+import { randomUUID } from "crypto"
 import { FastifyInstance } from "fastify"
 import { z } from 'zod'
-import { prisma } from "../../lib/prisma"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -14,8 +14,20 @@ export async function voteOnPoll(app: FastifyInstance) {
   
     const { pollId } = voteOnPollParams.parse(request.params)
     const { pollOptionId } = voteOnPollBody.parse(request.body)
-  
-    return reply.status(201).send()
-  
+
+    let { sessionId } = request.cookies
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      })
+    } 
+
+    return reply.status(201).send({ sessionId})
   })
 }
