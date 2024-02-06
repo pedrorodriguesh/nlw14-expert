0c3b4b063ca93228a310fcdcee77ac5ad3e2f8e8
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-06 19:24:14 -0300

    creating polls options

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -6,13 +6,23 @@ export async function createPoll(app: FastifyInstance) {
   app.post('/polls', async (request, reply) => {
     const createPollBodySchema = z.object({
       title: z.string(),
+      options: z.array(z.string())
     })
   
-    const { title } = createPollBodySchema.parse(request.body)
+    const { title, options } = createPollBodySchema.parse(request.body)
   
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map(option => {
+              return {
+                title: option
+              }
+            })
+          }
+        }
       }
     })
   
