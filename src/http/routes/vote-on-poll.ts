95c1046c29098f19df363060e5851a368c36d080
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-07 20:36:09 -0300

    starting incrementing and deleting votes with redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { randomUUID } from "crypto"
 import { FastifyInstance } from "fastify"
 import { z } from 'zod'
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -27,14 +28,16 @@ export async function voteOnPoll(app: FastifyInstance) {
           }
         }
       })
-      
+
       if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
         await prisma.vote.delete({
           where: {
             id: userPreviousVoteOnPoll.id
           }
         })
-        
+
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
           error: 'User has already voted on this poll'
@@ -60,6 +63,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     })
+
+    await redis.zincrby(pollId, 1, pollOptionId)
 
     return reply.status(201).send()
   })
