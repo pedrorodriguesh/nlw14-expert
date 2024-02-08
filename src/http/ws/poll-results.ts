15f7bc0bb3e344183cf51e9d5a6fe8677337b0bc
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-07 21:07:10 -0300

    pub sub pattern

--- src/http/ws/poll-results.ts
+++ src/http/ws/poll-results.ts
@@ -2,8 +2,8 @@ import { FastifyInstance } from "fastify";
 
 export async function pollResults(app: FastifyInstance) {
   app.get('/polls/:pollId/results', { websocket: true }, (connection, request) => {
-    connection.socket.on('message', (message: string) => {
-      connection.socket.send('you sent: ' + message)
-    })
+    
   })
 }
+
+// Pub/Sub => Publish and Subscribers => WebSockets
