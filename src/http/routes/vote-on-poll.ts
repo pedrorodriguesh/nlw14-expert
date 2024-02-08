2174ef96bdec945b99e15f2bf8d8c4119dc39f1a
Pedro Henrique Rodrigues <pedrohrodriguesofl@hotmail.com>
2024-02-07 21:12:20 -0300

    real time with redis and ws

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from "fastify"
 import { z } from 'zod'
 import { prisma } from "../../lib/prisma"
 import { redis } from "../../lib/redis"
+import { voting } from "../../utils/voting-pub-sub"
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -36,7 +37,13 @@ export async function voteOnPoll(app: FastifyInstance) {
           }
         })
 
-        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+        const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes)
+        
+        })
 
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({
@@ -64,7 +71,12 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
-    await redis.zincrby(pollId, 1, pollOptionId)
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
 
     return reply.status(201).send()
   })
